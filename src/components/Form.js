6dd25ac3d124c3d8b91cdb5e72c67dba67c6240a
david <davidealvarez1@gmail.com>
2020-02-27 13:51:56 -0800

    added formik, yup, and Form component

---
+++ src/components/Form.js
@@ -0,0 +1,15 @@
+import React from "react";
+import { Formik } from 'formik';
+
+const Form = () => {
+    return (
+        <div className="formDiv">
+            <h1>Form.js</h1>
+            <Formik>
+                
+            </Formik>
+        </div>
+    );
+}
+
+export default Form;
